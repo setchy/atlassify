818ecb3dc09602424fed3577793fb4046ef71f2e
Adam Setch <adam.setch@outlook.com>
2024-10-05 15:51:18 -0400

    build: remove unused locales
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

---
+++ scripts/remove-unused-locales.js
@@ -0,0 +1,36 @@
+const path = require('node:path');
+const fs = require('node:fs');
+
+const packageJson = require('../package.json');
+const electronLanguages = packageJson.build.electronLanguages;
+
+exports.default = async (context) => {
+  const appName = context.packager.appInfo.productFilename;
+  const appOutDir = context.appOutDir;
+
+  const resourcesPath = path.join(
+    appOutDir,
+    `${appName}.app`,
+    'Contents',
+    'Frameworks',
+    'Electron Framework.framework',
+    'Versions',
+    'A',
+    'Resources',
+  );
+
+  // Get all locale directories
+  const allLocales = fs
+    .readdirSync(resourcesPath)
+    .filter((file) => file.endsWith('.lproj'));
+
+  const langLocales = electronLanguages.map((lang) => `${lang}.lproj`);
+
+  // Remove unused locales
+  for (const locale of allLocales) {
+    if (!langLocales.includes(locale)) {
+      const localePath = path.join(resourcesPath, locale);
+      fs.rmSync(localePath, { recursive: true });
+    }
+  }
+};
