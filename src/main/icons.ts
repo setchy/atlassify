0e0535ffc28c025658697e3caf5215e125ba8983
Adam Setch <adam.setch@outlook.com>
2024-10-10 11:27:09 -0400

    test: coverage icons
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/main/icons.ts
+++ src/main/icons.ts
@@ -1,6 +1,5 @@
 import path from 'node:path';
 
-// Tray Icons
 export const idleIcon = getIconPath('tray-idleTemplate.png');
 export const idleAlternateIcon = getIconPath('tray-idle-white.png');
 export const activeIcon = getIconPath('tray-active.png');
