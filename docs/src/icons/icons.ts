2d787599f7da0461e0941c6cb6207b5ccee9fdaa
Adam Setch <adam.setch@outlook.com>
2024-11-09 21:32:42 -0500

    refactor: astro icon local
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

---
+++ docs/src/icons/icons.ts
@@ -0,0 +1,27 @@
+export const openSourceLibs = [
+  {
+    name: 'Electron',
+    link: 'https://electronjs.org/',
+    svg: 'electron',
+  },
+  {
+    name: 'React',
+    link: 'https://react.dev/',
+    svg: 'react',
+  },
+  {
+    name: 'Tailwind CSS',
+    link: 'https://tailwindcss.com/',
+    svg: 'tailwindcss',
+  },
+  {
+    name: 'Typescript',
+    link: 'https://typescriptlang.org/',
+    svg: 'typescript',
+  },
+  {
+    name: 'Atlassian',
+    link: 'https://atlassian.design/',
+    svg: 'atlassian',
+  },
+];
