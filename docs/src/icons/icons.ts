0353afd744d4a73a6f11c8ec9b2bce28519133b6
Adam Setch <adam.setch@outlook.com>
2024-11-09 21:56:12 -0500

    refactor: astro icon local (#339)
    * refactor: astro icon local
    
    Signed-off-by: Adam Setch <adam.setch@outlook.com>
    
    * refactor: astro icon local
    
    Signed-off-by: Adam Setch <adam.setch@outlook.com>
    
    * refactor: astro icon local
    
    Signed-off-by: Adam Setch <adam.setch@outlook.com>
    
    ---------
    
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

---
+++ docs/src/icons/icons.ts
@@ -0,0 +1,29 @@
+import type { IconDetails } from '~/types';
+
+export const openSourceLibs: IconDetails[] = [
+  {
+    name: 'Electron',
+    link: 'https://electronjs.org/',
+    svg: 'electron',
+  },
+  {
+    name: 'React',
+    link: 'https://react.dev/',
+    svg: 'react',
+  },
+  {
+    name: 'Tailwind CSS',
+    link: 'https://tailwindcss.com/',
+    svg: 'tailwindcss',
+  },
+  {
+    name: 'Typescript',
+    link: 'https://typescriptlang.org/',
+    svg: 'typescript',
+  },
+  {
+    name: 'Atlassian',
+    link: 'https://atlassian.design/',
+    svg: 'atlassian',
+  },
+];
