580cb21be5bb7367d1ec0f570e487ca34d61f2ea
Adam Setch <adam.setch@outlook.com>
2025-01-11 17:19:22 +0100

    feat(updates): log download progress and cancelled (#515)
    * feat(updates): log download progress and cancelled
    
    Signed-off-by: Adam Setch <adam.setch@outlook.com>
    
    * feat(updates): log download progress and cancelled
    
    Signed-off-by: Adam Setch <adam.setch@outlook.com>
    
    * feat(updates): log download progress and cancelled
    
    Signed-off-by: Adam Setch <adam.setch@outlook.com>
    
    ---------
    
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/main/updater.ts
+++ src/main/updater.ts
@@ -23,35 +23,59 @@ export default class Updater {
 
     autoUpdater.on('checking-for-update', () => {
       log.info('Auto Updater: Checking for update');
-      this.menuBuilder.setCheckForUpdatesMenuEnabled(false);
-    });
 
-    autoUpdater.on('error', (error) => {
-      log.error('Auto Updater: Error checking for new updates', error);
-      this.menuBuilder.setCheckForUpdatesMenuEnabled(true);
+      this.menuBuilder.setCheckForUpdatesMenuEnabled(false);
     });
 
     autoUpdater.on('update-available', () => {
       log.info('Auto Updater: New update available');
-      this.menuBuilder.setUpdateAvailableMenuEnabled(true);
+
       this.menubar.tray.setToolTip(
         `${APPLICATION.NAME}\nA new update is available`,
+      );
+      this.menuBuilder.setUpdateAvailableMenuEnabled(true);
+    });
+
+    autoUpdater.on('download-progress', (progressObj) => {
+      this.menubar.tray.setToolTip(
+        `${APPLICATION.NAME}\nDownloaded: ${progressObj.percent} %`,
       );
     });
 
     autoUpdater.on('update-downloaded', () => {
       log.info('Auto Updater: Update downloaded, ready to install');
-      this.menuBuilder.setUpdateReadyForInstallMenuEnabled(true);
+
       this.menubar.tray.setToolTip(
         `${APPLICATION.NAME}\nA new update is ready to install`,
       );
+      this.menuBuilder.setUpdateReadyForInstallMenuEnabled(true);
     });
 
     autoUpdater.on('update-not-available', () => {
       log.info(
         'Auto Updater: No updates available, already using latest version.',
       );
-      this.menuBuilder.setCheckForUpdatesMenuEnabled(true);
+
+      this.resetState();
     });
+
+    autoUpdater.on('update-cancelled', () => {
+      log.warn('Auto Updater: Update cancelled');
+
+      this.resetState();
+    });
+
+    autoUpdater.on('error', (error) => {
+      log.error('Auto Updater: Error checking for new updates', error);
+
+      this.resetState();
+    });
+  }
+
+  private resetState() {
+    this.menubar.tray.setToolTip(APPLICATION.NAME);
+    this.menuBuilder.setCheckForUpdatesMenuEnabled(true);
+    this.menuBuilder.setUpdateAvailableMenuEnabled(false);
+    this.menuBuilder.setUpdateReadyForInstallMenuEnabled(false);
   }
 }
