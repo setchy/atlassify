42791ec3f97547bebf4d3959cdb2fb35b6f00ea8
Adam Setch <adam.setch@outlook.com>
2025-02-01 14:25:45 -0500

    docs: fix flickering
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -6,6 +6,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    build: {
+            cssCodeSplit: false, // Ensures all CSS is bundled together
+    },
     plugins: [tailwindcss()],
   },
   integrations: [icon()],
