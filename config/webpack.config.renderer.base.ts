4810c9400a43792a36a9b98b09e3fb94bc5b0faf
Adam Setch <adam.setch@outlook.com>
2025-03-12 12:27:42 -0400

    refactor: preload context bridge
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- config/webpack.config.renderer.base.ts
+++ config/webpack.config.renderer.base.ts
@@ -1,106 +1,106 @@
-import path from "node:path";
-import CopyWebpackPlugin from "copy-webpack-plugin";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import type webpack from "webpack";
-import { merge } from "webpack-merge";
+import path from 'node:path';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import type webpack from 'webpack';
+import { merge } from 'webpack-merge';
 
-import baseConfig from "./webpack.config.common";
-import webpackPaths from "./webpack.paths";
+import baseConfig from './webpack.config.common';
+import webpackPaths from './webpack.paths';
 
-import twemoji from "@discordapp/twemoji";
-import { Constants } from "../src/renderer/utils/constants";
-import { Errors } from "../src/renderer/utils/errors";
+import twemoji from '@discordapp/twemoji';
+import { Constants } from '../src/renderer/utils/constants';
+import { Errors } from '../src/renderer/utils/errors';
 
 const ALL_EMOJIS = [
-	...Constants.ALL_READ_EMOJIS,
-	...Errors.BAD_CREDENTIALS.emojis,
-	...Errors.BAD_REQUEST.emojis,
-	...Errors.NETWORK.emojis,
-	...Errors.UNKNOWN.emojis,
+  ...Constants.ALL_READ_EMOJIS,
+  ...Errors.BAD_CREDENTIALS.emojis,
+  ...Errors.BAD_REQUEST.emojis,
+  ...Errors.NETWORK.emojis,
+  ...Errors.UNKNOWN.emojis,
 ];
 
 export const ALL_EMOJI_SVG_FILENAMES = ALL_EMOJIS.map((emoji) => {
-	const imgHtml = twemoji.parse(emoji, { folder: "svg", ext: ".svg" });
-	return extractSvgFilename(imgHtml);
+  const imgHtml = twemoji.parse(emoji, { folder: 'svg', ext: '.svg' });
+  return extractSvgFilename(imgHtml);
 });
 
 const configuration: webpack.Configuration = {
-	devtool: "inline-source-map",
+  devtool: 'inline-source-map',
 
-	mode: "development",
+  mode: 'development',
 
-	target: ["web", "electron-renderer"],
+  target: ['web', 'electron-renderer'],
 
-	entry: [path.join(webpackPaths.srcRendererPath, "index.tsx")],
+  entry: [path.join(webpackPaths.srcRendererPath, 'index.tsx')],
 
-	output: {
-		path: webpackPaths.buildPath,
-		filename: "renderer.js",
-		library: {
-			type: "umd",
-		},
-	},
+  output: {
+    path: webpackPaths.buildPath,
+    filename: 'renderer.js',
+    library: {
+      type: 'umd',
+    },
+  },
 
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: [
-					MiniCssExtractPlugin.loader, // Extract CSS into a separate file
-					"css-loader", // Translates CSS into CommonJS
-					"postcss-loader", // Automatically uses the postcss.config.js file
-				],
-			},
-		],
-	},
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, // Extract CSS into a separate file
+          'css-loader', // Translates CSS into CommonJS
+          'postcss-loader', // Automatically uses the postcss.config.js file
+        ],
+      },
+    ],
+  },
 
-	plugins: [
-		// Extract CSS into a separate file
-		new MiniCssExtractPlugin({
-			filename: "styles.css", // Output file for the CSS
-		}),
+  plugins: [
+    // Extract CSS into a separate file
+    new MiniCssExtractPlugin({
+      filename: 'styles.css', // Output file for the CSS
+    }),
 
-		// Generate HTML file with script and link tags injected
-		new HtmlWebpackPlugin({
-			filename: path.join("index.html"),
-			template: path.join(webpackPaths.srcRendererPath, "index.html"),
-			minify: {
-				collapseWhitespace: true,
-				removeAttributeQuotes: true,
-				removeComments: true,
-			},
-			isBrowser: false,
-		}),
+    // Generate HTML file with script and link tags injected
+    new HtmlWebpackPlugin({
+      filename: path.join('index.html'),
+      template: path.join(webpackPaths.srcRendererPath, 'index.html'),
+      minify: {
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
+        removeComments: true,
+      },
+      isBrowser: false,
+    }),
 
-		// Twemoji SVGs for Emoji parsing
-		new CopyWebpackPlugin({
-			patterns: [
-				{
-					from: path.join(
-						webpackPaths.nodeModulesPath,
-						"@discordapp/twemoji",
-						"dist",
-						"svg",
-					),
-					to: "images/twemoji",
-					// Only copy the SVGs for the emojis we use
-					filter: (resourcePath) => {
-						return ALL_EMOJI_SVG_FILENAMES.some((filename) =>
-							resourcePath.endsWith(`/${filename}`),
-						);
-					},
-				},
-			],
-		}),
-	],
+    // Twemoji SVGs for Emoji parsing
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.join(
+            webpackPaths.nodeModulesPath,
+            '@discordapp/twemoji',
+            'dist',
+            'svg',
+          ),
+          to: 'images/twemoji',
+          // Only copy the SVGs for the emojis we use
+          filter: (resourcePath) => {
+            return ALL_EMOJI_SVG_FILENAMES.some((filename) =>
+              resourcePath.endsWith(`/${filename}`),
+            );
+          },
+        },
+      ],
+    }),
+  ],
 };
 
 function extractSvgFilename(imgHtml: string) {
-	const srcMatch = /src="(.*)"/.exec(imgHtml);
-	const src = srcMatch ? srcMatch[1] : "";
-	const filename = src.split("/").pop(); // Get the last part after splitting by "/"
-	return filename;
+  const srcMatch = /src="(.*)"/.exec(imgHtml);
+  const src = srcMatch ? srcMatch[1] : '';
+  const filename = src.split('/').pop(); // Get the last part after splitting by "/"
+  return filename;
 }
 
 export default merge(baseConfig, configuration);
