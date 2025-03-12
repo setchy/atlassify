20c560d6aeb4bb2cbcbc9e838a7c4d5d46a62468
Adam Setch <adam.setch@outlook.com>
2025-03-12 12:26:50 -0400

    refactor: preload context bridge
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/renderer/utils/emojis.ts
+++ src/renderer/utils/emojis.ts
@@ -1,41 +1,14 @@
-import twemoji, { type TwemojiOptions } from "@discordapp/twemoji";
+import twemoji, { type TwemojiOptions } from '@discordapp/twemoji';
 
-import { Constants } from "./constants";
-import { Errors } from "./errors";
-
-const EMOJI_FORMAT = "svg";
-
-const ALL_EMOJIS = [
-	...Constants.ALL_READ_EMOJIS,
-	...Errors.BAD_CREDENTIALS.emojis,
-	...Errors.BAD_REQUEST.emojis,
-	...Errors.NETWORK.emojis,
-	...Errors.UNKNOWN.emojis,
-];
-
-export const ALL_EMOJI_SVG_FILENAMES = ALL_EMOJIS.map(async (emoji) => {
-	const imgHtml = convertTextToEmojiImgHtml(emoji);
-	return extractSvgFilename(await imgHtml);
-});
+const EMOJI_FORMAT = 'svg';
 
 export async function convertTextToEmojiImgHtml(text: string): Promise<string> {
-	// return text;
+  const directory = await window.atlassify.twemojiDirectory();
 
-	const directory = await window.atlassify.twemojiDirectory();
-
-	console.log("ADAM DIR", directory);
-
-	return twemoji.parse(text, {
-		folder: EMOJI_FORMAT,
-		callback: (icon: string, _options: TwemojiOptions) => {
-			return `${directory}/${icon}.${EMOJI_FORMAT}`;
-		},
-	});
-}
-
-function extractSvgFilename(imgHtml: string): string {
-	const srcMatch = /src="(.*)"/.exec(imgHtml);
-	const src = srcMatch ? srcMatch[1] : "";
-	const filename = src.split("/").pop(); // Get the last part after splitting by "/"
-	return filename;
+  return twemoji.parse(text, {
+    folder: EMOJI_FORMAT,
+    callback: (icon: string, _options: TwemojiOptions) => {
+      return `${directory}/${icon}.${EMOJI_FORMAT}`;
+    },
+  });
 }
--- src/shared/events.ts
+++ src/shared/events.ts
@@ -1,41 +1,41 @@
 export type EventType =
-	| "atlassify:icon-idle"
-	| "atlassify:icon-active"
-	| "atlassify:icon-error"
-	| "atlassify:quit"
-	| "atlassify:window-show"
-	| "atlassify:window-hide"
-	| "atlassify:version"
-	| "atlassify:update-title"
-	| "atlassify:use-alternate-idle-icon"
-	| "atlassify:update-keyboard-shortcut"
-	| "atlassify:update-auto-launch"
-	| "atlassify:safe-storage-encrypt"
-	| "atlassify:safe-storage-decrypt"
-	| "atlassify:notification-sound-path"
-	| "atlassify:open-external"
-	| "atlassify:reset-app"
-	| "atlassify:update-theme"
-	| "atlassify:twemoji-directory";
+  | 'atlassify:icon-idle'
+  | 'atlassify:icon-active'
+  | 'atlassify:icon-error'
+  | 'atlassify:quit'
+  | 'atlassify:window-show'
+  | 'atlassify:window-hide'
+  | 'atlassify:version'
+  | 'atlassify:update-title'
+  | 'atlassify:use-alternate-idle-icon'
+  | 'atlassify:update-keyboard-shortcut'
+  | 'atlassify:update-auto-launch'
+  | 'atlassify:safe-storage-encrypt'
+  | 'atlassify:safe-storage-decrypt'
+  | 'atlassify:notification-sound-path'
+  | 'atlassify:open-external'
+  | 'atlassify:reset-app'
+  | 'atlassify:update-theme'
+  | 'atlassify:twemoji-directory';
 
 export interface IAutoLaunch {
-	openAtLogin: boolean;
-	openAsHidden: boolean;
+  openAtLogin: boolean;
+  openAsHidden: boolean;
 }
 
 export interface IKeyboardShortcut {
-	enabled: boolean;
-	keyboardShortcut: string;
+  enabled: boolean;
+  keyboardShortcut: string;
 }
 
 export interface IOpenExternal {
-	url: string;
-	activate: boolean;
+  url: string;
+  activate: boolean;
 }
 
 export type EventData =
-	| string
-	| boolean
-	| IKeyboardShortcut
-	| IAutoLaunch
-	| IOpenExternal;
+  | string
+  | boolean
+  | IKeyboardShortcut
+  | IAutoLaunch
+  | IOpenExternal;
