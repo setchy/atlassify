57449c351cb518b19bd39d792cfe994589dd0092
Adam Setch <adam.setch@outlook.com>
2025-03-15 21:48:23 -0400

    fix: attempt at fixing login on windows
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/renderer/routes/Login.tsx
+++ src/renderer/routes/Login.tsx
@@ -2,11 +2,18 @@ import { type FC, Fragment, useCallback, useContext, useState } from 'react';
 
 import ButtonGroup from '@atlaskit/button/button-group';
 import Button from '@atlaskit/button/new';
-import ErrorIcon from '@atlaskit/icon/core/error';
+import Form, {
+  ErrorMessage,
+  Field,
+  FormFooter,
+  FormSection,
+  HelperMessage,
+} from '@atlaskit/form';
 import LinkExternalIcon from '@atlaskit/icon/core/link-external';
 import LogInIcon from '@atlaskit/icon/core/log-in';
 import LockLockedIcon from '@atlaskit/icon/utility/lock-locked';
-import { Box, Inline, Stack } from '@atlaskit/primitives';
+import { Box, Inline } from '@atlaskit/primitives';
+import TextField from '@atlaskit/textfield';
 import Tooltip from '@atlaskit/tooltip';
 
 import { useNavigate } from 'react-router-dom';
@@ -44,143 +51,117 @@ export const LoginRoute: FC = () => {
         setIsValidToken(false);
       }
     },
-    [login, navigate],
+    [login],
   );
-
-  const labelClasses = 'font-bold text-sm text-gray-700';
-  const requiredClasses = 'text-red-500';
-  const inputClasses = 'border border-gray-400 rounded px-2 py-1 w-full';
-  const helpTextClasses = 'text-xs text-gray-500 mt-1';
-  const errorClasses = 'text-red-500 text-xs mt-1';
 
   return (
     <Fragment>
       <Header>Login with Atlassian</Header>
 
       <Box paddingInline="space.400">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            const formData = new FormData(e.currentTarget);
-            const data = {
-              username: formData.get('username') as Username,
-              token: formData.get('token') as Token,
-            };
-            loginUser(data);
-          }}
-        >
-          <Stack space="space.200">
-            <Stack space="space.050">
-              <Inline alignBlock="center" space="space.050">
-                <label htmlFor="username" className={labelClasses}>
-                  Username
-                </label>
-                <span className={requiredClasses}>*</span>
-              </Inline>
-
-              <Box>
-                <input
-                  type="text"
-                  id="username"
+        <Form<IValues> onSubmit={loginUser}>
+          {({ formProps, submitting }) => (
+            <form {...formProps}>
+              <FormSection>
+                <Field
+                  aria-required={true}
                   name="username"
-                  className={inputClasses}
-                />
-              </Box>
-
-              <Box>
-                <div className={helpTextClasses}>
-                  Your Atlassian username / email address
-                </div>
-              </Box>
-            </Stack>
-
-            <Stack space="space.050">
-              <Inline alignBlock="center" space="space.050">
-                <label htmlFor="token" className={labelClasses}>
-                  API Token
-                </label>
-                <span className={requiredClasses}>*</span>
-              </Inline>
-
-              <Box>
-                <input
-                  type="password"
-                  id="token"
+                  label="Username"
+                  defaultValue={''}
+                  // FIXME: isRequired causes the renderer process on Windows devices to crash upon mouse enter
+                  // isRequired
+                  testId="login-username"
+                >
+                  {({ fieldProps }) => (
+                    <Fragment>
+                      <TextField autoComplete="off" {...fieldProps} />
+                      <HelperMessage>
+                        Your Atlassian username / email address
+                      </HelperMessage>
+                    </Fragment>
+                  )}
+                </Field>
+                <Field
+                  aria-required={true}
                   name="token"
-                  className={inputClasses}
-                />
-              </Box>
+                  label="API Token"
+                  defaultValue={''}
+                  // FIXME: isRequired causes the renderer process on Windows devices to crash upon mouse enter
+                  // isRequired
+                  testId="login-token"
+                >
+                  {({ fieldProps }) => (
+                    <Fragment>
+                      <TextField type="password" {...fieldProps} />
+                      <HelperMessage>
+                        <Inline alignBlock="center" space="space.050">
+                          <Tooltip content="Create an API Token">
+                            <Button
+                              appearance="discovery"
+                              spacing="compact"
+                              iconBefore={LockLockedIcon}
+                              onClick={() => openAtlassianCreateToken()}
+                              testId="login-create-token"
+                            >
+                              Create an API Token
+                            </Button>
+                          </Tooltip>
+                          <Box>for your account and paste above</Box>
+                        </Inline>
+                      </HelperMessage>
+                    </Fragment>
+                  )}
+                </Field>
+              </FormSection>
 
-              <Inline alignBlock="center" space="space.050">
-                <Tooltip content="Create an API Token">
-                  <Button
-                    appearance="discovery"
-                    spacing="compact"
-                    iconBefore={LockLockedIcon}
-                    onClick={() => openAtlassianCreateToken()}
-                    testId="login-create-token"
-                  >
-                    Create an API Token
-                  </Button>
-                </Tooltip>
-                <Box>
-                  <div className={helpTextClasses}>
-                    for your account and paste above
-                  </div>
-                </Box>
-              </Inline>
-            </Stack>
-          </Stack>
-
-          {/* Error message for invalid token */}
-          <Box paddingBlock="space.100">
-            {!isValidToken && (
-              <div className={errorClasses}>
-                <Inline space="space.100" alignBlock="center">
-                  <ErrorIcon label="Login error" />
-                  <Box>
+              <Box paddingBlock="space.050">
+                {!isValidToken && (
+                  <ErrorMessage>
                     Oops! The username + token combination provided are not
                     valid. Please try again.
-                  </Box>
-                </Inline>
-              </div>
-            )}
-
-            <Box paddingBlockStart="space.300">
-              <Inline alignBlock="center" spread="space-between">
-                <Box>
-                  <Tooltip content="See Atlassian documentation" position="top">
-                    <Button
-                      appearance="subtle"
-                      iconBefore={LinkExternalIcon}
-                      onClick={() => openAtlassianSecurityDocs()}
-                      testId="login-docs"
+                  </ErrorMessage>
+                )}
+                <Inline alignBlock="center" spread="space-between">
+                  <Box paddingBlockStart="space.300">
+                    <Tooltip
+                      content="See Atlassian documentation"
+                      position="top"
                     >
-                      Docs
-                    </Button>
-                  </Tooltip>
-                </Box>
-                <ButtonGroup label="Form submit options">
-                  <Button
-                    appearance="subtle"
-                    onClick={() => navigate(-1)}
-                    testId="login-cancel"
-                  >
-                    Cancel
-                  </Button>
-                  <Button
-                    type="submit"
-                    iconBefore={LogInIcon}
-                    appearance="primary"
-                    testId="login-submit"
-                  >
-                    Login
-                  </Button>
-                </ButtonGroup>
-              </Inline>
-            </Box>
-          </Box>
-        </form>
+                      <Button
+                        appearance="subtle"
+                        iconBefore={LinkExternalIcon}
+                        onClick={() => openAtlassianSecurityDocs()}
+                        testId="login-docs"
+                      >
+                        Docs
+                      </Button>
+                    </Tooltip>
+                  </Box>
+                  <FormFooter>
+                    <ButtonGroup label="Form submit options">
+                      <Button
+                        appearance="subtle"
+                        onClick={() => navigate(-1)}
+                        testId="login-cancel"
+                      >
+                        Cancel
+                      </Button>
+                      <Button
+                        type="submit"
+                        iconBefore={LogInIcon}
+                        appearance="primary"
+                        isLoading={submitting}
+                        testId="login-submit"
+                      >
+                        Login
+                      </Button>
+                    </ButtonGroup>
+                  </FormFooter>
+                </Inline>
+              </Box>
+            </form>
+          )}
+        </Form>
       </Box>
     </Fragment>
   );
