296a23dd4a7ab88a848b5aae355f557abd6f65fa
Adam Setch <adam.setch@outlook.com>
2025-04-12 17:23:49 +1000

    feat: add i18n support
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/renderer/components/settings/AppearanceSettings.tsx
+++ src/renderer/components/settings/AppearanceSettings.tsx
@@ -1,181 +1,181 @@
-import { type FC, useCallback, useContext, useEffect, useState } from "react";
+import { type FC, useCallback, useContext, useEffect, useState } from 'react';
 
-import { IconButton, SplitButton } from "@atlaskit/button/new";
-import Heading from "@atlaskit/heading";
-import RetryIcon from "@atlaskit/icon/core/retry";
-import ZoomInIcon from "@atlaskit/icon/core/zoom-in";
-import ZoomOutIcon from "@atlaskit/icon/core/zoom-out";
-import { Box, Inline, Stack, Text, xcss } from "@atlaskit/primitives";
-import { RadioGroup } from "@atlaskit/radio";
-import type { OptionsPropType } from "@atlaskit/radio/dist/types/types";
-import { setGlobalTheme } from "@atlaskit/tokens";
-import Tooltip from "@atlaskit/tooltip";
+import { IconButton, SplitButton } from '@atlaskit/button/new';
+import Heading from '@atlaskit/heading';
+import RetryIcon from '@atlaskit/icon/core/retry';
+import ZoomInIcon from '@atlaskit/icon/core/zoom-in';
+import ZoomOutIcon from '@atlaskit/icon/core/zoom-out';
+import { Box, Inline, Stack, Text, xcss } from '@atlaskit/primitives';
+import { RadioGroup } from '@atlaskit/radio';
+import type { OptionsPropType } from '@atlaskit/radio/dist/types/types';
+import { setGlobalTheme } from '@atlaskit/tokens';
+import Tooltip from '@atlaskit/tooltip';
 
-import Select from "@atlaskit/select";
-import { useTranslation } from "react-i18next";
-import { AppContext } from "../../context/App";
-import { Theme } from "../../types";
-import { setTheme } from "../../utils/theme";
-import { zoomLevelToPercentage, zoomPercentageToLevel } from "../../utils/zoom";
+import Select from '@atlaskit/select';
+import { useTranslation } from 'react-i18next';
+import { AppContext } from '../../context/App';
+import { Theme } from '../../types';
+import { setTheme } from '../../utils/theme';
+import { zoomLevelToPercentage, zoomPercentageToLevel } from '../../utils/zoom';
 
 let zoomResizeTimeout: NodeJS.Timeout;
 const ZOOM_RESIZE_DELAY = 200;
 
 export const AppearanceSettings: FC = () => {
-	const { settings, updateSetting } = useContext(AppContext);
+  const { settings, updateSetting } = useContext(AppContext);
 
-	const [zoomPercentage, setZoomPercentage] = useState(
-		zoomLevelToPercentage(window.atlassify.zoom.getLevel()),
-	);
+  const [zoomPercentage, setZoomPercentage] = useState(
+    zoomLevelToPercentage(window.atlassify.zoom.getLevel()),
+  );
 
-	const zoomBoxStyles = xcss({
-		backgroundColor: "color.background.accent.gray.subtlest",
-	});
+  const zoomBoxStyles = xcss({
+    backgroundColor: 'color.background.accent.gray.subtlest',
+  });
 
-	useEffect(() => {
-		window.atlassify.onSystemThemeUpdate((updatedTheme: Theme) => {
-			if (settings.theme === Theme.SYSTEM) {
-				setTheme(updatedTheme);
-				setGlobalTheme({ colorMode: "auto" });
-			}
-		});
-	}, [settings.theme]);
+  useEffect(() => {
+    window.atlassify.onSystemThemeUpdate((updatedTheme: Theme) => {
+      if (settings.theme === Theme.SYSTEM) {
+        setTheme(updatedTheme);
+        setGlobalTheme({ colorMode: 'auto' });
+      }
+    });
+  }, [settings.theme]);
 
-	window.addEventListener("resize", () => {
-		// clear the timeout
-		clearTimeout(zoomResizeTimeout);
-		// start timing for event "completion"
-		zoomResizeTimeout = setTimeout(() => {
-			const zoomPercentage = zoomLevelToPercentage(
-				window.atlassify.zoom.getLevel(),
-			);
-			setZoomPercentage(zoomPercentage);
-			updateSetting("zoomPercentage", zoomPercentage);
-		}, ZOOM_RESIZE_DELAY);
-	});
+  window.addEventListener('resize', () => {
+    // clear the timeout
+    clearTimeout(zoomResizeTimeout);
+    // start timing for event "completion"
+    zoomResizeTimeout = setTimeout(() => {
+      const zoomPercentage = zoomLevelToPercentage(
+        window.atlassify.zoom.getLevel(),
+      );
+      setZoomPercentage(zoomPercentage);
+      updateSetting('zoomPercentage', zoomPercentage);
+    }, ZOOM_RESIZE_DELAY);
+  });
 
-	const themeOptions: OptionsPropType = [
-		{
-			name: "theme",
-			label: "System",
-			value: Theme.SYSTEM,
-			testId: "theme-system",
-		},
-		{
-			name: "theme",
-			label: "Light",
-			value: Theme.LIGHT,
-			testId: "theme-light",
-		},
-		{ name: "theme", label: "Dark", value: Theme.DARK, testId: "theme-dark" },
-	];
+  const themeOptions: OptionsPropType = [
+    {
+      name: 'theme',
+      label: 'System',
+      value: Theme.SYSTEM,
+      testId: 'theme-system',
+    },
+    {
+      name: 'theme',
+      label: 'Light',
+      value: Theme.LIGHT,
+      testId: 'theme-light',
+    },
+    { name: 'theme', label: 'Dark', value: Theme.DARK, testId: 'theme-dark' },
+  ];
 
-	const { i18n } = useTranslation();
+  const { i18n } = useTranslation();
 
-	const languages = [
-		{ label: "English", value: "en" },
-		{ label: "Deutsch", value: "de" },
-		{ label: "Español", value: "es" },
-		{ label: "Français", value: "fr" },
-	];
+  const languages = [
+    { label: 'English', value: 'en' },
+    { label: 'Deutsch', value: 'de' },
+    { label: 'Español', value: 'es' },
+    { label: 'Français', value: 'fr' },
+  ];
 
-	const handleLanguageChange = useCallback(
-		(option) => {
-			i18n.changeLanguage(option.value);
-		},
-		[i18n],
-	);
+  const handleLanguageChange = useCallback(
+    (option) => {
+      i18n.changeLanguage(option.value);
+    },
+    [i18n],
+  );
 
-	return (
-		<Stack space="space.100">
-			<Heading size="small">Appearance</Heading>
+  return (
+    <Stack space="space.100">
+      <Heading size="small">Appearance</Heading>
 
-			<Box paddingInlineStart="space.050">
-				<Inline space="space.100" alignBlock="start">
-					<Text id="language-label" weight="medium">
-						Language:
-					</Text>
-					<Select
-						options={languages}
-						onChange={handleLanguageChange}
-						testId="settings-language-selector"
-					/>
-				</Inline>
-			</Box>
+      <Box paddingInlineStart="space.050">
+        <Inline space="space.100" alignBlock="start">
+          <Text id="language-label" weight="medium">
+            Language:
+          </Text>
+          <Select
+            options={languages}
+            onChange={handleLanguageChange}
+            testId="settings-language-selector"
+          />
+        </Inline>
+      </Box>
 
-			<Box paddingInlineStart="space.050">
-				<Inline space="space.100" alignBlock="start">
-					<Text id="theme-label" weight="medium">
-						Theme:
-					</Text>
-					<RadioGroup
-						options={themeOptions}
-						defaultValue={settings.theme}
-						value={settings.theme}
-						onChange={(evt) => {
-							updateSetting("theme", evt.target.value as Theme);
-						}}
-						aria-labelledby="theme-label"
-					/>
-				</Inline>
-			</Box>
+      <Box paddingInlineStart="space.050">
+        <Inline space="space.100" alignBlock="start">
+          <Text id="theme-label" weight="medium">
+            Theme:
+          </Text>
+          <RadioGroup
+            options={themeOptions}
+            defaultValue={settings.theme}
+            value={settings.theme}
+            onChange={(evt) => {
+              updateSetting('theme', evt.target.value as Theme);
+            }}
+            aria-labelledby="theme-label"
+          />
+        </Inline>
+      </Box>
 
-			<Box paddingInlineStart="space.050">
-				<Inline space="space.100" alignBlock="center">
-					<Text id="theme-label" weight="medium">
-						Zoom:
-					</Text>
-					<Inline xcss={zoomBoxStyles}>
-						<SplitButton spacing="compact">
-							<Inline alignBlock="center">
-								<Box paddingInline="space.150">
-									<Text>{zoomPercentage.toFixed(0)}%</Text>
-								</Box>
-								<Tooltip content="Zoom out" position="bottom">
-									<IconButton
-										label="Zoom out"
-										icon={ZoomOutIcon}
-										shape="circle"
-										spacing="compact"
-										onClick={() =>
-											zoomPercentage > 0 &&
-											window.atlassify.zoom.setLevel(
-												zoomPercentageToLevel(zoomPercentage - 10),
-											)
-										}
-										testId="settings-zoom-out"
-									/>
-								</Tooltip>
-								<Tooltip content="Zoom in" position="bottom">
-									<IconButton
-										label="Zoom in"
-										icon={ZoomInIcon}
-										shape="circle"
-										spacing="compact"
-										onClick={() =>
-											zoomPercentage < 120 &&
-											window.atlassify.zoom.setLevel(
-												zoomPercentageToLevel(zoomPercentage + 10),
-											)
-										}
-										testId="settings-zoom-in"
-									/>
-								</Tooltip>
-							</Inline>
-							<Tooltip content="Reset zoom" position="bottom">
-								<IconButton
-									label="Reset zoom"
-									icon={RetryIcon}
-									shape="circle"
-									spacing="compact"
-									onClick={() => window.atlassify.zoom.setLevel(0)}
-									testId="settings-zoom-reset"
-								/>
-							</Tooltip>
-						</SplitButton>
-					</Inline>
-				</Inline>
-			</Box>
-		</Stack>
-	);
+      <Box paddingInlineStart="space.050">
+        <Inline space="space.100" alignBlock="center">
+          <Text id="theme-label" weight="medium">
+            Zoom:
+          </Text>
+          <Inline xcss={zoomBoxStyles}>
+            <SplitButton spacing="compact">
+              <Inline alignBlock="center">
+                <Box paddingInline="space.150">
+                  <Text>{zoomPercentage.toFixed(0)}%</Text>
+                </Box>
+                <Tooltip content="Zoom out" position="bottom">
+                  <IconButton
+                    label="Zoom out"
+                    icon={ZoomOutIcon}
+                    shape="circle"
+                    spacing="compact"
+                    onClick={() =>
+                      zoomPercentage > 0 &&
+                      window.atlassify.zoom.setLevel(
+                        zoomPercentageToLevel(zoomPercentage - 10),
+                      )
+                    }
+                    testId="settings-zoom-out"
+                  />
+                </Tooltip>
+                <Tooltip content="Zoom in" position="bottom">
+                  <IconButton
+                    label="Zoom in"
+                    icon={ZoomInIcon}
+                    shape="circle"
+                    spacing="compact"
+                    onClick={() =>
+                      zoomPercentage < 120 &&
+                      window.atlassify.zoom.setLevel(
+                        zoomPercentageToLevel(zoomPercentage + 10),
+                      )
+                    }
+                    testId="settings-zoom-in"
+                  />
+                </Tooltip>
+              </Inline>
+              <Tooltip content="Reset zoom" position="bottom">
+                <IconButton
+                  label="Reset zoom"
+                  icon={RetryIcon}
+                  shape="circle"
+                  spacing="compact"
+                  onClick={() => window.atlassify.zoom.setLevel(0)}
+                  testId="settings-zoom-reset"
+                />
+              </Tooltip>
+            </SplitButton>
+          </Inline>
+        </Inline>
+      </Box>
+    </Stack>
+  );
 };
--- src/renderer/i18n/index.ts
+++ src/renderer/i18n/index.ts
@@ -1,48 +1,48 @@
-import i18n from "i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import { initReactI18next } from "react-i18next";
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
 
-import deTranslation from "./locales/de.json";
-import enTranslation from "./locales/en.json";
-import esTranslation from "./locales/es.json";
-import frTranslation from "./locales/fr.json";
+import deTranslation from './locales/de.json';
+import enTranslation from './locales/en.json';
+import esTranslation from './locales/es.json';
+import frTranslation from './locales/fr.json';
 
 const resources = {
-	de: {
-		translation: deTranslation,
-	},
-	en: {
-		translation: enTranslation,
-	},
-	es: {
-		translation: esTranslation,
-	},
-	fr: {
-		translation: frTranslation,
-	},
+  de: {
+    translation: deTranslation,
+  },
+  en: {
+    translation: enTranslation,
+  },
+  es: {
+    translation: esTranslation,
+  },
+  fr: {
+    translation: frTranslation,
+  },
 };
 
 i18n
-	// detect user language
-	.use(LanguageDetector)
-	// pass the i18n instance to react-i18next
-	.use(initReactI18next)
-	// init i18next
-	.init({
-		resources,
-		fallbackLng: "de",
-		debug: process.env.NODE_ENV === "development",
-		interpolation: {
-			escapeValue: false, // not needed for react as it escapes by default
-		},
-		detection: {
-			// order and from where user language should be detected
-			order: ["localStorage", "navigator"],
-			// keys or params to lookup language from
-			lookupLocalStorage: "atlassify_language",
-			// cache user language
-			caches: ["localStorage"],
-		},
-	});
+  // detect user language
+  .use(LanguageDetector)
+  // pass the i18n instance to react-i18next
+  .use(initReactI18next)
+  // init i18next
+  .init({
+    resources,
+    fallbackLng: 'de',
+    debug: process.env.NODE_ENV === 'development',
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+    detection: {
+      // order and from where user language should be detected
+      order: ['localStorage', 'navigator'],
+      // keys or params to lookup language from
+      lookupLocalStorage: 'atlassify_language',
+      // cache user language
+      caches: ['localStorage'],
+    },
+  });
 
 export default i18n;
