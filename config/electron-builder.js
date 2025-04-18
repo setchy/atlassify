e764320fbcdfcd6bf6b607e20c3478d091940a07
Adam Setch <adam.setch@outlook.com>
2025-04-18 15:57:00 -0700

    build: prepare `v1.17.0` release (#877)
    * 1.17.0
    
    * downgrade electron-builder
    
    Signed-off-by: Adam Setch <adam.setch@outlook.com>
    
    ---------
    
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- config/electron-builder.js
+++ config/electron-builder.js
@@ -22,7 +22,7 @@ const config = {
     icon: 'assets/images/app-icon.icns',
     identity: 'Adam Setch (5KD23H9729)',
     type: 'distribution',
-    notarize: false,
+    notarize: false, // Handle notarization in afterSign.js
     target: {
       target: 'default',
       arch: ['universal', 'arm64', 'x64'],
