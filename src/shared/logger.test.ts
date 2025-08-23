bc93c69faf8293492d8610e12e20c2d6848aab01
Adam Setch <adam.setch@outlook.com>
2025-08-23 09:09:34 -0400

    refactor: shared module
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/shared/logger.test.ts
+++ src/shared/logger.test.ts
@@ -21,11 +21,13 @@ describe('shared/logger.ts', () => {
       expect(logInfoSpy).toHaveBeenCalledTimes(1);
       expect(logInfoSpy).toHaveBeenCalledWith('[foo]', 'bar');
     });
+
     it('logs info with single context', () => {
       logInfo('foo', 'bar', ['ctx']);
       expect(logInfoSpy).toHaveBeenCalledTimes(1);
       expect(logInfoSpy).toHaveBeenCalledWith('[foo]', 'bar', '[ctx]');
     });
+
     it('logs info with multiple contexts', () => {
       logInfo('foo', 'bar', ['ctx1', 'ctx2']);
       expect(logInfoSpy).toHaveBeenCalledTimes(1);
@@ -39,10 +41,17 @@ describe('shared/logger.ts', () => {
       expect(logWarnSpy).toHaveBeenCalledTimes(1);
       expect(logWarnSpy).toHaveBeenCalledWith('[foo]', 'bar');
     });
-    it('logs warn with contexts', () => {
+
+    it('logs warn with single context', () => {
       logWarn('foo', 'bar', ['ctx']);
       expect(logWarnSpy).toHaveBeenCalledTimes(1);
       expect(logWarnSpy).toHaveBeenCalledWith('[foo]', 'bar', '[ctx]');
+    });
+
+    it('logs warn with multiple contexts', () => {
+      logWarn('foo', 'bar', ['ctx1', 'ctx2']);
+      expect(logWarnSpy).toHaveBeenCalledTimes(1);
+      expect(logWarnSpy).toHaveBeenCalledWith('[foo]', 'bar', '[ctx1 >> ctx2]');
     });
   });
 
@@ -52,13 +61,25 @@ describe('shared/logger.ts', () => {
       expect(logErrorSpy).toHaveBeenCalledTimes(1);
       expect(logErrorSpy).toHaveBeenCalledWith('[foo]', 'bar', mockError);
     });
-    it('logs error with contexts', () => {
+
+    it('logs error with single context', () => {
       logError('foo', 'bar', mockError, ['ctx']);
       expect(logErrorSpy).toHaveBeenCalledTimes(1);
       expect(logErrorSpy).toHaveBeenCalledWith(
         '[foo]',
         'bar',
         '[ctx]',
+        mockError,
+      );
+    });
+
+    it('logs error with multiple contexts', () => {
+      logError('foo', 'bar', mockError, ['ctx1', 'ctx2']);
+      expect(logErrorSpy).toHaveBeenCalledTimes(1);
+      expect(logErrorSpy).toHaveBeenCalledWith(
+        '[foo]',
+        'bar',
+        '[ctx1 >> ctx2]',
         mockError,
       );
     });
