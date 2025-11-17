18c944380e79d52629763b948ea9083cf5fc7d31
Adam Setch <adam.setch@outlook.com>
2025-11-17 13:07:43 -0500

    refactor: test suite helpers and mocks
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/renderer/routes/Notifications.test.tsx
+++ src/renderer/routes/Notifications.test.tsx
@@ -1,5 +1,5 @@
-import { mockAccountNotifications } from '../__mocks__/notifications-mocks';
 import { renderWithAppContext } from '../__helpers__/test-utils';
+import { mockAccountNotifications } from '../__mocks__/notifications-mocks';
 import { Errors } from '../utils/errors';
 import { NotificationsRoute } from './Notifications';
 
