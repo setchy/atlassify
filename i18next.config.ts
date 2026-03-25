a6932c6ea3d9801e1c704d1cc26c7f71e388b0e4
Adam Setch <adam.setch@outlook.com>
2026-03-25 11:45:14 -0400

    feat(locales): i18n cli
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- i18next.config.ts
+++ i18next.config.ts
@@ -5,11 +5,22 @@ export default defineConfig({
   extract: {
     input: ['src/renderer/**/*.{ts,tsx}'],
     output: 'src/renderer/i18n/locales/{{language}}.json',
+
     defaultNS: false,
     primaryLanguage: 'en',
+
     sort: true,
     removeUnusedKeys: true,
     indentation: 2,
     disablePlurals: true,
+  },
+  lint: {
+    ignore: ['**/*.test.{ts,tsx}'],
+  },
+
+  types: {
+    input: ['src/renderer/i18n/locales/en.json'],
+    output: 'src/@types/i18next.d.ts',
+    resourcesFile: 'src/@types/resources.d.ts',
   },
 });
